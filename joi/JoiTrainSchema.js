7013073fbd045e030074aa730295cb2709d75412
HaxReid <bmaury018@gmail.com>
2023-12-12 15:09:41 +0100

    update with joi

--- joi/JoiTrainSchema.js
+++ joi/JoiTrainSchema.js
@@ -1,18 +1,9 @@
 import Joi from 'joi';
-import Stations from "../models/Stations.js";
-
-const existingStation = async (value, helpers) => {
-    const station = await Stations.findOne({name: value});
-    if (!station) {
-        return helpers.error('Cette station n\'existe pas', { value });
-    }
-    return value;
-}
 
 const JoiTrainSchema = Joi.object({
     name: Joi.string().required(),
-    start_station: Joi.string().required().custom(existingStation , 'Station exists validation'),
-    end_station: Joi.string().required().custom(existingStation , 'Station exists validation'),
+    start_station: Joi.string().required(),
+    end_station: Joi.string().required(),
     time_of_departure: Joi.date().optional()
 });
 
